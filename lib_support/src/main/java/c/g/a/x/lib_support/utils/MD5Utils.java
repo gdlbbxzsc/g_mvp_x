4be36326a406926a8d9fe6f16f3ed6ffc94a52b3
gdlbbxzsc <gdlbbxzsc@163.com>
2019-12-11 10:42:55 +0800

    test

--- lib_support/src/main/java/c/g/a/x/lib_support/utils/MD5Utils.java
+++ lib_support/src/main/java/c/g/a/x/lib_support/utils/MD5Utils.java
@@ -14,11 +14,11 @@ public final class MD5Utils {
 
         StringBuffer md5StrBuff = new StringBuffer();
 
-        for (int i = 0; i < byteArray.length; i++) {
-            if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)
-                md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+        for (byte b : byteArray) {
+            if (Integer.toHexString(0xFF & b).length() == 1)
+                md5StrBuff.append("0").append(Integer.toHexString(0xFF & b));
             else
-                md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+                md5StrBuff.append(Integer.toHexString(0xFF & b));
         }
         return md5StrBuff.toString();
 
@@ -27,14 +27,14 @@ public final class MD5Utils {
     public static String encrypt2(byte[] source) {
         String s = null;
 
-        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(source);
-            byte tmp[] = md.digest();
+            byte[] tmp = md.digest();
 
-            char str[] = new char[16 * 2];
+            char[] str = new char[16 * 2];
 
             int k = 0;
             for (int i = 0; i < 16; i++) {
--- lib_support/src/main/java/c/g/a/x/lib_support/views/view/FlowLayout.java
+++ lib_support/src/main/java/c/g/a/x/lib_support/views/view/FlowLayout.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 
 import c.g.a.x.lib_support.R;
-import c.g.a.x.lib_support.utils.MoneyHelper;
 
 
 @SuppressWarnings("unused")
