da5105c6b08ee3a67b21eaa6f98f86b4278e5c9a
Gdl <gdlbbxzsc@163.com>
2020-01-10 15:01:29 +0800

    G

--- lib_support/src/main/java/c/g/a/x/lib_support/views/dialog/DialogQueueHelper.java
+++ lib_support/src/main/java/c/g/a/x/lib_support/views/dialog/DialogQueueHelper.java
@@ -6,11 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class DialogQueueHelper<T extends Dialog> {
+public final class DialogQueueHelper<T extends Dialog> {
 
     private final Queue<T> dialogQueue = new LinkedList<>();
 
     private T currentDialog = null;//当前显示的Dialog
+
+    private boolean pause;
 
     public static DialogQueueHelper getInstance() {
         return InnerInstance.INSTANCE;
@@ -23,18 +25,18 @@ public class DialogQueueHelper<T extends Dialog> {
     private DialogQueueHelper() {
     }
 
-    public void addDialog(List<T> dialogs) {
+    public final void addDialog(List<T> dialogs) {
         for (T dialog : dialogs) {
             if (dialog == null) continue;
             dialogQueue.offer(dialog);
         }
     }
 
-    public void addDialog(T dialog) {
+    public final void addDialog(T dialog) {
         if (dialog != null) dialogQueue.offer(dialog);
     }
 
-    public void show() {
+    public final void show() {
         if (currentDialog != null) return;
 
         //从队列中拿出一个Dialog实例,并从列表中移除
@@ -43,6 +45,11 @@ public class DialogQueueHelper<T extends Dialog> {
         //当队列为空的时候拿出来的会是null
         if (currentDialog == null) return;
 
+        if (pause) {
+            pause = false;
+            return;
+        }
+
         currentDialog.show();
         currentDialog.setOnDismissListener(dialog -> {
             //这边设置了dismiss监听,在监听回调中再次调用show方法,可以获取下一个弹窗
@@ -50,4 +57,13 @@ public class DialogQueueHelper<T extends Dialog> {
             show();
         });
     }
+
+    public final void pause() {
+        pause = true;
+    }
+
+    public final void clear() {
+        dialogQueue.clear();
+    }
+
 }
