5441655b04f44f5ec37ea300a3b1fbd6f95c9800
Gdl <gdlbbxzsc@163.com>
2020-06-05 14:41:08 +0800

    mqtt and chat activity

--- module_chat/src/main/java/c/g/a/x/module_chat/detail/adapter/TimeViewHolder.java
+++ module_chat/src/main/java/c/g/a/x/module_chat/detail/adapter/TimeViewHolder.java
@@ -4,7 +4,7 @@ import c.g.a.x.lib_support.views.adapter.v2.abslistview.ViewHolder;
 import c.g.a.x.module_chat.R;
 import c.g.a.x.module_chat.databinding.AdapterChatTimeBinding;
 
-
+//这里应该根据两条数据间的时间差和 当前系统时间间隔展示不同的时间
 public class TimeViewHolder extends ViewHolder<String, AdapterChatTimeBinding> {
 
 
