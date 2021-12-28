43aee3346018ee19e38b82f64ab880f6b3e872d5
Gdl <gdlbbxzsc@163.com>
2021-12-28 14:52:24 +0800

    mqtt and chat activity

---
+++ lib_support/src/main/java/c/g/a/x/lib_support/android/utils/Watermark.java
@@ -0,0 +1,113 @@
+package c.g.a.x.lib_support.android.utils;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+// Watermark.getInstance().show(this, "测试水印");
+public class Watermark {
+
+    private static volatile Watermark sInstance;
+
+
+    public static Watermark getInstance() {
+        if (sInstance == null) {
+            synchronized (Watermark.class) {
+                if (sInstance == null) {
+                    sInstance = new Watermark();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    private Watermark() {
+    }
+
+    public void show(Activity activity, String text) {
+        WatermarkDrawable drawable = new WatermarkDrawable();
+        drawable.mText = text;
+
+        ViewGroup rootView = activity.findViewById(android.R.id.content);
+        FrameLayout layout = new FrameLayout(activity);
+        layout.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.setBackground(drawable);
+        rootView.addView(layout);
+    }
+
+    private class WatermarkDrawable extends Drawable {
+        private Paint mPaint;
+
+        private String mText;
+
+
+        private WatermarkDrawable() {
+            mPaint = new Paint();
+            mPaint.setColor(0xAEAEAEAE);
+            mPaint.setTextSize(50);
+            mPaint.setAntiAlias(true);
+        }
+
+        @Override
+        public void draw(@NonNull Canvas canvas) {
+            int width = getBounds().right;
+            int height = getBounds().bottom;
+
+            float textWidth = mPaint.measureText(mText) + 20;
+            float wh = (float) Math.sqrt(textWidth * textWidth / 2);
+
+
+            canvas.drawColor(0x00000000);//背景色
+
+            // 右上角
+//            canvas.translate(width - wh - 20, 50);
+//            canvas.rotate(45);//角度
+//            canvas.drawText(mText, 0, 0, mPaint);
+
+
+            canvas.translate(width - width / 2, -width / 2);
+            canvas.rotate(-35);//角度
+
+            int diagonal = (int) Math.sqrt(width * width + height * height); // 对角线的长度
+
+
+            int startx = width / 4;
+            int stepx = (int) (startx + textWidth);
+            int stepy = height / 10;
+
+            int index = 0;
+            float fromX;
+            for (int positionY = stepy; positionY <= diagonal; positionY += stepy) {
+                fromX = -diagonal + (index++ % 2) * startx;
+                for (float positionX = fromX; positionX <= diagonal; positionX += stepx) {
+                    canvas.drawText(mText, positionX, positionY, mPaint);
+                }
+            }
+
+        }
+
+        @Override
+        public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
+        }
+
+        @Override
+        public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        }
+
+        @Override
+        public int getOpacity() {
+            return PixelFormat.TRANSLUCENT;
+        }
+
+    }
+}
